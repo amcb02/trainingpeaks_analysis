b85cc80d8125ca504590b3182b654b11d8cebe2a
Andy McBurney <amcburney02@gmail.com>
2023-04-21 11:41:03 -0400

    Commit 2

--- Analysis1.R
+++ Analysis1.R
@@ -12,3 +12,19 @@ library(FITfileR)
 install.packages("leaflet")
 library(leaflet)
 
+workouts <- read_csv("/Users/andrewmcburney/Desktop/workouts 2.csv")%>%
+  select(-c(c("HRZone6Minutes":"HRZone10Minutes"),
+            c("TorqueAverage":"TorqueMax"),
+            c("PWRZone7Minutes":"PWRZone10Minutes")))%>%
+  mutate(completed = as.logical(case_when(
+    is.na(TimeTotalInHours) == F ~ T,
+    is.na(TimeTotalInHours) == T ~ F)))
+
+bikes <- workouts%>%
+  filter(WorkoutType == "Bike")
+
+runs <- workouts%>%
+  filter(WorkoutType == "Run")
+
+swims <- workouts%>%
+  filter(WorkoutType == "Swim")
